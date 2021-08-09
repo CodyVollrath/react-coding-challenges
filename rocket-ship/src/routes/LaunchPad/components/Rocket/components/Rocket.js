821dabc26db461ab1252056a8ce269bfd4c2c6db
Cody Vollrath <950doenut.buff@gmail.com>
2021-08-09 18:12:59 -0400

    Edited Rocket ship functional side

--- rocket-ship/src/routes/LaunchPad/components/Rocket/components/Rocket.js
+++ rocket-ship/src/routes/LaunchPad/components/Rocket/components/Rocket.js
@@ -1,24 +1,9 @@
 import React, { useState, Component } from 'react';
 import RocketCore from './RocketCore';
 
-export function FunctionalRocket() {
+//TODO Work Here
+export const FunctionalRocket = () =>  {
   const [initialLaunchTime] = useState(Date.now());
 
   return <RocketCore initialLaunchTime={initialLaunchTime} />;
-}
-
-export class ClassRocket extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      initialLaunchTime: Date.now()
-    };
-  }
-
-  render() {
-    const { initialLaunchTime } = this.state;
-
-    return <RocketCore initialLaunchTime={initialLaunchTime} />;
-  }
 }
